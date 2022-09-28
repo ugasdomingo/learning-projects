3b3efc6809430039bc3763a444499789d0f654d2
Domingo Ugas <ugasdomingo@gmail.com>
2022-09-28 17:42:47 -0400

    First Commit

---
+++ frontend-tailwing-react/src/App.tsx
@@ -0,0 +1,34 @@
+const App = () => {
+	return (
+		<div>
+			<nav>
+				<a href="#">Digiempresas</a>
+				<div>
+					<a href="#">home</a>
+					<a href="#">shop</a>
+					<a href="#">about</a>
+					<a href="#">contact</a>
+				</div>
+			</nav>
+			<header>
+				<img src="" alt="banner" />
+				<article>
+					<h1>Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing
+						elit. Perspiciatis in cupiditate facere iusto ad facilis
+						earum ut tenetur, praesentium, eveniet adipisci aperiam
+						sunt, repellendus blanditiis cum et officia minima vero!
+					</p>
+					<button>Shop now</button>
+					<div>
+						<button>prev</button>
+						<button>next</button>
+					</div>
+				</article>
+			</header>
+		</div>
+	);
+};
+
+export default App;
